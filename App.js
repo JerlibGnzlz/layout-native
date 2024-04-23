715e31a29ba694432fe52d9865a337fc63785316
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-04-22 22:00:14 -0300

    fix del proyecto scroll view

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { View, StyleSheet, Image, ScrollView, Text } from 'react-native';
 const App = () => {
   return (
     <>
-      <View>
+      <ScrollView>
         <View style={{ flexDirection: "row" }}>
           <Image
             style={styles.banner}
@@ -15,28 +15,28 @@ const App = () => {
         <View style={styles.contenedor}>
           <Text style={styles.titulo}>Que hacer en Paris</Text>
           <ScrollView horizontal>
-            <View style={{ flexDirection: "row" }}>
+            <View >
               <Image
                 style={styles.ciudad}
                 source={require("./assets/img/actividad1.jpg")}
               />
             </View>
 
-            <View style={{ flexDirection: "row" }}>
+            <View >
               <Image
                 style={styles.ciudad}
                 source={require("./assets/img/actividad2.jpg")}
               />
             </View>
 
-            <View style={{ flexDirection: "row" }}>
+            <View>
               <Image
                 style={styles.ciudad}
                 source={require("./assets/img/actividad3.jpg")}
               />
             </View>
 
-            <View style={{ flexDirection: "row" }}>
+            <View>
               <Image
                 style={styles.ciudad}
                 source={require("./assets/img/actividad4.jpg")}
@@ -50,8 +50,79 @@ const App = () => {
               />
             </View>
           </ScrollView>
+
+          <Text style={styles.titulo}>Los mejores alojamientos</Text>
+          <View>
+            <View >
+              <Image
+                style={styles.mejores}
+                source={require("./assets/img/mejores1.jpg")}
+              />
+            </View>
+
+            <View >
+              <Image
+                style={styles.mejores}
+                source={require("./assets/img/mejores2.jpg")}
+              />
+            </View>
+
+            <View >
+              <Image
+                style={styles.mejores}
+                source={require("./assets/img/mejores3.jpg")}
+              />
+            </View>
+
+            <View >
+              <Image
+                style={styles.mejores}
+                source={require("./assets/img/mejores1.jpg")}
+              />
+            </View>
+          </View>
+
+          <Text style={styles.titulo}>Hospedaje en los Angeles</Text>
+
+          <View style={styles.listado}>
+            <View style={styles.listadoItem} >
+              <Image
+                style={styles.mejores}
+                source={require("./assets/img/hospedaje1.jpg")}
+              />
+              <Text>Casa 3 recamaras</Text>
+            </View>
+
+            <View style={styles.listadoItem}>
+              <Image
+                style={styles.mejores}
+                source={require("./assets/img/hospedaje2.jpg")}
+              />
+              <Text>Living Amplio</Text>
+            </View>
+
+
+            <View style={styles.listadoItem}>
+              <Image
+                style={styles.mejores}
+                source={require("./assets/img/hospedaje3.jpg")}
+              />
+              <Text>Sala Grande</Text>
+            </View>
+
+
+            <View style={styles.listadoItem}>
+              <Image
+                style={styles.mejores}
+                source={require("./assets/img/hospedaje4.jpg")}
+              />
+              <Text>Cocina Grande</Text>
+            </View>
+
+
+          </View>
         </View>
-      </View>
+      </ScrollView>
     </>
   );
 };
@@ -73,6 +144,25 @@ const styles = StyleSheet.create({
     width: 250,
     height: 250,
     marginRight: 5
+  },
+  mejores: {
+    width: "100%",
+    height: 200,
+    marginVertical: 5
+  },
+  hospedaje: {
+    width: 200,
+    height: 200,
+  },
+  listado: {
+    flexDirection: 'row',
+    flexWrap: "wrap",
+    justifyContent: "space-between"
+  },
+
+  listadoItem: {
+    flexBasis: "49%",
+
   }
 
 });
