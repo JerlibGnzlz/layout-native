67a42132ac04a16a58b6446f90850fc94f55551d
jerlibgnzlz <jerlibgnzlz@gmail.com>
2024-04-22 21:07:53 -0300

    subiendo proyecto

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { View, StyleSheet, Image } from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <View>
+        <View style={{ flexDirection: "row" }}>
+          <Image
+            style={styles.banner}
+            source={require("./assets/img/bg.jpg")}
+          />
+        </View>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  banner: {
+    height: 250,
+    flex: 1
+  }
+});
+
+export default App;
